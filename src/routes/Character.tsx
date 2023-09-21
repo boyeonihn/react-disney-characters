2e6356e9a55012392e25863d723ec48d6d846670
Boyeon Ihn <boyeonihn@gmail.com>
2023-09-21 17:46:32 +0200

    feat: build Character route page

--- src/routes/Character.tsx
+++ src/routes/Character.tsx
@@ -1,8 +1,51 @@
-import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { useLocation, useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { CharacterInterface } from '.';
+import { Header } from '../components';
+import { AvatarPic } from '../components/Avatar';
+
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
 
 export const Character = () => {
   const { id } = useParams();
+  const [character, setCharacter] = useState<CharacterInterface>();
+  const [loading, setLoading] = useState(true);
 
-  console.log(id);
-  return <h1>Character: {id}</h1>;
+  const {
+    state: { name },
+  } = useLocation();
+
+  console.log('### location', { location });
+
+  const loadCharacter = async () => {
+    const res = await fetch(
+      `https://disney_api.nomadcoders.workers.dev/characters/${id}`
+    );
+    const json = await res.json();
+    console.log('#### json', { json });
+    setCharacter(json);
+  };
+
+  useEffect(() => {
+    loadCharacter();
+    setLoading(false);
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        'LOADING... '
+      ) : (
+        <Container>
+          <Header title={name || 'loading...'} />
+          <AvatarPic src={character?.imageUrl} />
+        </Container>
+      )}
+    </div>
+  );
 };
