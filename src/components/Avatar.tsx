58eab9d4ba6d0db0a3a6d1b279d411e92deb9c41
Boyeon Ihn <boyeonihn@gmail.com>
2023-09-21 17:46:13 +0200

    feat: apply styled styling to Avatar component

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -2,20 +2,36 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { CharacterInterface } from '../routes';
 
-const AvatarPic = styled.img`
+const AvatarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding: 20px;
+  border: 1px solid gold;
+  background-color: gold;
+`;
+
+export const AvatarPic = styled.img`
   border-radius: 50%;
   object-fit: cover;
-  width: 120px;
-  height: 120px;
+  width: 150px;
+  height: 150px;
   vertical-align: middle;
+`;
+
+const AvatarName = styled.h3`
+  color: ${(props) => props.theme.accentColor};
 `;
 
 export default function Avatar({ id, name, imageUrl }: CharacterInterface) {
   return (
     <div>
-      <Link to={String(id)}>
-        <AvatarPic src={imageUrl || ''} alt={`Picture of ${name}`} />
-        <h3>{name}</h3>
+      <Link to={`/character/${String(id)}`} state={{ name }}>
+        <AvatarContainer>
+          <AvatarPic src={imageUrl || ''} alt={`Picture of ${name}`} />
+          <AvatarName>{name}</AvatarName>
+        </AvatarContainer>
       </Link>
     </div>
   );
